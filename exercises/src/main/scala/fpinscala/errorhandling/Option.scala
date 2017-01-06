661c57c67be413c327a35983c8fa5d414c3fdafa
hongdi.ren <hongdi.ren@dianping.com>
2017-01-06 14:21:06 +0800

    option

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,20 +1,31 @@
 package fpinscala.errorhandling
 
 
-import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
+import java.util.regex.{Pattern, PatternSyntaxException}
+
+import scala.{Either => _, Option => _, Some => _}
+// hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = sys.error("todo")
+  def map[B](f: A => B): Option[B] = this match {
+    case s: Some[A] => Some(f(s.get))
+    case None => None
+  }
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(v) => v
+    case None => default
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f).getOrElse(None)
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
+  def orElse[B >: A](ob: => Option[B]): Option[B] = map(Some(_)).getOrElse(ob)
 
-  def filter(f: A => Boolean): Option[A] = sys.error("todo")
+  def filter(f: A => Boolean): Option[A] = flatMap(v => if (f(v)) this else None)
 }
+
 case class Some[+A](get: A) extends Option[A]
+
 case object None extends Option[Nothing]
 
 object Option {
@@ -24,7 +35,9 @@ object Option {
       val x = 42 + 5
       x + y
     }
-    catch { case e: Exception => 43 } // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
+    catch {
+      case e: Exception => 43
+    } // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
   }
 
   def failingFn2(i: Int): Int = {
@@ -32,17 +45,48 @@ object Option {
       val x = 42 + 5
       x + ((throw new Exception("fail!")): Int) // A thrown Exception can be given any type; here we're annotating it with the type `Int`
     }
-    catch { case e: Exception => 43 }
+    catch {
+      case e: Exception => 43
+    }
   }
 
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) flatMap { m =>
+      mean(xs.map(x => math.pow(x - m, 2)))
+    }
+  }
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a flatMap (i => b map (f(i, _)))
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
+  def pattern(s: String): Option[Pattern] =
+    try {
+      Some(Pattern.compile(s))
+    } catch {
+      case _: PatternSyntaxException => None
+    }
+
+  def bothMatch_2(pat1: String, pat2: String, s: String): Option[Boolean] =
+    map2(pattern(pat1), pattern(pat2))((p1, p2) => p1.matcher(s).matches() && p2.matcher(s).matches())
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    a.foldLeft[Option[List[A]]](Some(Nil))((acc, op) => map2(acc, op)(_ :+ _))
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a.foldLeft[Option[List[B]]](Some(Nil))((op, a) => op.flatMap(list => f(a).map(list :+ _)))
+
+  def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] =
+    traverse(a)(identity)
+
+  def main(args: Array[String]): Unit = {
+    println(s"sequence: ${sequence(List(Some(1), Some(2), Some(3)))}")
+    println(s"sequence: ${sequence(List(Some(1), None, Some(3)))}")
+
+    println(s"sequenceViaTraverse: ${sequenceViaTraverse(List(Some(1), Some(2), Some(3)))}")
+    println(s"sequenceViaTraverse: ${sequenceViaTraverse(List(Some(1), None, Some(3)))}")
+  }
 }
