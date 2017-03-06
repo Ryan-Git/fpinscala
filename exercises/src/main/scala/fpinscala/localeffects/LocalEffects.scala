2c0389ecd76aaaa5a100e337796e2137050d592e
hongdi.ren <hongdi.ren@dianping.com>
2017-03-06 16:39:18 +0800

    LocalEffects: partition

--- exercises/src/main/scala/fpinscala/localeffects/LocalEffects.scala
+++ exercises/src/main/scala/fpinscala/localeffects/LocalEffects.scala
@@ -128,7 +128,22 @@ object STArray {
 object Immutable {
   def noop[S] = ST[S,Unit](())
 
-  def partition[S](a: STArray[S,Int], l: Int, r: Int, pivot: Int): ST[S,Int] = ???
+  def partition[S](a: STArray[S,Int], l: Int, r: Int, pivot: Int): ST[S,Int] = for {
+    vp <- a.read(pivot)
+    _ <- a.swap(pivot, r)
+    j <- STRef(l)
+    _ <- (l until r).foldLeft(noop[S])((s, i) => for {
+      _ <- s
+      vi <- a.read(i)
+      _  <- if (vi < vp) for {
+        vj <- j.read
+        _ <- a.swap(i, vj)
+        _ <- j.write(vj + 1)
+      } yield () else noop[S]
+    } yield ())
+    x <- j.read
+    _ <- a.swap(x, r)
+  } yield x
 
   def qs[S](a: STArray[S,Int], l: Int, r: Int): ST[S, Unit] = for {
     pi <- partition(a, l, r, l + (r - l) / 2)
